9c64f91f5964727b1c51d6803af4ad90cd83bfbe
Jesse Leppens <jesseleppens@hotmail.com>
2022-10-03 14:13:00 +0200

    added if statement when searching for a trip

--- Station-Frontend/src/app/trip-home/trip-home.component.ts
+++ Station-Frontend/src/app/trip-home/trip-home.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TripsService} from "../Services/trips.service";
 import {Trip} from "../Models/trip";
-import {FormBuilder} from "@angular/forms";
 import {TitleCasePipe} from "@angular/common";
 
 @Component({
@@ -11,19 +10,16 @@ import {TitleCasePipe} from "@angular/common";
 })
 export class TripHomeComponent implements OnInit {
 
-  constructor(private tripsService: TripsService, private titlecasePipe:TitleCasePipe) {
+  constructor(private tripsService: TripsService, private titlecasePipe: TitleCasePipe) {
   }
 
   Trips: Trip[]
 
   ngOnInit(): void {
-    this.tripsService.getTrips("Rijen", "Tilburg").subscribe({
-      next: value => this.Trips = value.trips
-    })
   }
 
-  fromStation: string = ""
-  toStation: string = ""
+  fromStation: string
+  toStation: string
 
   onSubmit() {
     this.fromStation = this.titlecasePipe.transform(this.fromStation)
